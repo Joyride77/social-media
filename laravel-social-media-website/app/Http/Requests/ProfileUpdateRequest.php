6c18590e74a5d125f3308592f699756f581f3d26
Joyride77 <nfive8@gmail.com>
2024-05-15 14:33:43 +0800

    Unique user

--- laravel-social-media-website/app/Http/Requests/ProfileUpdateRequest.php
+++ laravel-social-media-website/app/Http/Requests/ProfileUpdateRequest.php
@@ -17,7 +17,25 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', 'regex:/^[\w\-\.]+$/i'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+        ];
+    }
+
+    /**
+     * Get the custom messages for validator errors.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'username.regex' => 'Username can only contain alphanumeric characters, dash (-) and dot (.)',
+            'name.required' => 'The name field is required.',
+            'username.required' => 'The username field is required.',
+            'email.required' => 'The email field is required.',
+            'email.email' => 'The email must be a valid email address.',
+            'email.unique' => 'The email has already been taken.',
         ];
     }
 }
