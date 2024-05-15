d05d1b125fed4ba8f344100e7d593ac4f6a9d979
Joyride77 <nfive8@gmail.com>
2024-05-15 17:06:09 +0800

    Use AuthenticatedLayout on home to display navbar

--- laravel-social-media-website/resources/js/Pages/Home.vue
+++ laravel-social-media-website/resources/js/Pages/Home.vue
@@ -6,12 +6,7 @@ import GroupList from '@/Components/app/Group/GroupList.vue';
 import FollowingList from '@/Components/app/Following/FollowingList.vue';
 import PostList from '@/Components/app/Post/PostList.vue';
 import CreatePost from '@/Components/app/Post/CreatePost.vue';
-
-defineProps({
-    posts: Object,
-    groups: Array,
-    followings: Array
-});
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 
 </script>
 
@@ -19,19 +14,19 @@ defineProps({
 
     <Head title="Social Media Website" />
 
-    <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
-        <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
-            <GroupList />
+    <AuthenticatedLayout>
+        <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
+            <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
+                <GroupList />
+            </div>
+            <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
+                <FollowingList />
+            </div>
+            <div class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col">
+                <CreatePost />
+                <PostList class="flex-1" />
+            </div>
         </div>
-        <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
-            <FollowingList />
-        </div>
-        <div class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col">
-            <CreatePost />
-            <PostList class="flex-1" />
-        </div>
-
-
-    </div>
+    </AuthenticatedLayout>
 
 </template>
